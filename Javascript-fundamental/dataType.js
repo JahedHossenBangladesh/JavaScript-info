44c9288cff3911e2ce259f8609f3058e59036e41
JahedHossenBangladesh <jahedhossen4358.com>
2021-01-15 01:26:25 +0600

    Code Structure use strict , variable , data type task is done

---
+++ Javascript-fundamental/dataType.js
@@ -0,0 +1,37 @@
+const message = "there are 8 basic data type";
+let n = 12.22;
+const SpecialNumericValues = "Infinity ,-Infinity and NaN";
+// alert(Infinity); // Infinity
+// alert("not a number" / 2); // NaN
+
+const bigInt = 1234567890123456789012345678901234567890n;
+
+//String
+const firstName = "Jahed ";
+const lastName = "Hossen";
+const fullName = `My name is ${firstName} ${lastName} `;
+console.log(fullName);
+console.log(`Total = ${1 + 2}`);
+//Boolean
+let hasHouse = true;
+let hasMoney = false;
+let hasGirlFriend = 4 < 2;
+console.log(hasGirlFriend);
+let representNothing = null;
+console.log(representNothing);
+
+let age;
+console.log(age);
+age = 22;
+age = undefined;
+console.log(age);
+
+// Single value container is call primitive data type
+const knowTheType = typeof 0;
+console.log(knowTheType);
+
+//Task
+let name = "Jahed";
+console.log(`age ${22}`); // 22
+console.log(`hello ${"name"}`); // hello name
+console.log(`My name is ${name}`); //My name is Jahed
