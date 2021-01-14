b10d1fea880465e173e30b3b98351127c367ca9a
JahedHossenBangladesh <jahedhossen4358.com>
2021-01-15 00:56:31 +0600

    the task is done

---
+++ Javascript-fundamental/variableTask.js
@@ -0,0 +1,11 @@
+let admin, name;
+name = "John";
+admin = name;
+// alert(admin);
+console.log(admin);
+
+let planetName;
+let currentVisitor;
+
+const BIRTHDAY = "19.2.2202";
+// const age = someCode(BIRTHDAY);
