e5ad4cbfffd161cde99dbc7238c3ba2569d8edf2
JahedHossenBangladesh <jahedhossen4358.com>
2021-01-15 18:59:16 +0600

    the reminder is use in the file

---
+++ Javascript-fundamental/basicOperators.js
@@ -0,0 +1,12 @@
+// the unary operator
+let x = 1;
+x = -x;
+console.log(x);
+
+x = 3;
+let y = 2;
+console.log(x - y);
+
+// the math  will be addition Subtraction Multiplication Reminder Exponentiation
+// Reminder %
+console.log(5 % 2);
