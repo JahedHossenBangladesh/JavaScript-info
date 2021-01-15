5bc928cf34ffb98e180127728b375849b75494d8
JahedHossenBangladesh <jahedhossen4358.com>
2021-01-15 19:01:31 +0600

    The exponentiation

--- Javascript-fundamental/basicOperators.js
+++ Javascript-fundamental/basicOperators.js
@@ -10,3 +10,5 @@ console.log(x - y);
 // the math  will be addition Subtraction Multiplication Reminder Exponentiation
 // Reminder %
 console.log(5 % 2);
+// Exponentiation
+console.log("2 multiplied 4 times ", 2 ** 4);
