40531128fce142e1ec6a247b69c26e04d14fe7f2
JahedHossenBangladesh <jahedhossen4358.com>
2021-01-21 00:47:55 +0600

    for and while loops the task is remain

--- Javascript-fundamental/loopsWhileFor.js
+++ Javascript-fundamental/loopsWhileFor.js
@@ -1,5 +1,24 @@
 let i = 0;
 while (i < 4) {
-  console.log(i);
+  console.log("for the while", i);
   i++;
+}
+for (let i = 0; i < 4; i++) {
+  console.log("for the for", i);
+}
+do {
+  console.log("do while", i);
+  i++;
+} while (i < 3);
+let v = 0;
+for (; v < 9; v++) {
+  console.log("for loop skipping part", v);
+}
+for (let p = 0; p < 10; p++) {
+  if (p % 2 == 0) continue;
+  console.log("continue to the next iteration", p);
+  // continue help decrease nesting {}
+  //  if(p%2 ==0){
+  //   console.log(p);
+  // }
 }
