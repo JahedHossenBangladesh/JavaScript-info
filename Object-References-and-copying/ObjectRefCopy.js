d96ce715bc5d982d5b76d4414055deaafbd94a1a
JahedHossenBangladesh <jahedhossen4358.com>
2021-01-31 00:32:04 +0600

    Cloning and merging , object.assign

--- Object-References-and-copying/ObjectRefCopy.js
+++ Object-References-and-copying/ObjectRefCopy.js
@@ -13,4 +13,17 @@ console.log(userDetail);
 let a = {};
 let b = {};
 let c = a == b;
-console.log(c);
+console.log(c); // false
+
+let newUser = {
+  name: "sokina",
+  age: 20,
+};
+let clone = {};
+
+for (key in newUser) {
+  clone[key] = newUser[key];
+}
+clone.age = 40;
+clone.name = "Banuni";
+console.log(clone);
