7d07e8b83701af8a570ba91acabd7ed7e35caa04
JahedHossenBangladesh <jahedhossen4358.com>
2021-02-03 00:47:08 +0600

    interlinked object

--- object-the-Basic/GarbageCollection.js
+++ object-the-Basic/GarbageCollection.js
@@ -2,3 +2,24 @@ let user = {
   name: "mehedi",
 };
 user = null;
+
+function marry(man, woman) {
+  woman.husband = man;
+  man.wife = woman;
+  return {
+    father: man,
+    mother: woman,
+  };
+}
+let family = marry(
+  {
+    name: "john",
+  },
+  {
+    name: "jugoni",
+  }
+);
+console.log(family);
+
+delete family.father;
+console.log(family);
