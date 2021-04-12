fe9e875b5af48dd9b016f9872ad3434a774af6c7
JahedHossenBangladesh <jahedhossen4358.com>
2021-04-13 02:40:27 +0600

    This is String

---
+++ Data Type/String.js
@@ -0,0 +1,21 @@
+let single = 'Single-quoted';
+let double = "double-quoted";
+let backticks = `backticks`;
+
+
+function sum(a,b) {
+    return a +b;
+}
+
+console.log(`1 + 2 = ${sum(1,2)}`)
+
+let guestList = `Guests:
+*John
+*Pete
+*marry`;
+console.log(guestList);
+
+let guestLi= "Guests *John";
+
+let  guest = "Guests:\n * John\n * Pete\n * Mary";
+console.log(guest);
