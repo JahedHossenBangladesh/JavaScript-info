0fc3bbd4e274f041bd6ca3855d034c0080034851
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-06-30 02:00:52 +0600

    Type Conversions

---
+++ Type Conversions/TypeConversions.js
@@ -0,0 +1,30 @@
+// String Conversion
+let value = true;
+console.log(typeof value);//boolean
+
+value = String(value);
+console.log(typeof value);
+
+// Numeric Conversions
+
+console.log(typeof "234")
+let numm = Number(33);
+numm=Number(true)
+
+console.log( numm);
+numm = Number(false)
+console.log(numm)
+
+numm = Number('3333a');
+console.log(numm);
+
+// Boolean Conversion
+
+console.log(Boolean(1));
+console.log(Boolean(0));
+console.log(Boolean("hello"));
+console.log(Boolean(''))
+
+// the string with zero is true
+console.log(Boolean('0'));
+
