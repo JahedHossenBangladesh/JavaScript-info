86068d3c14add831a6503bed76f7aeed26661db3
JahedHossenBangladesh <mokbulhossen413@gmail.com>
2021-07-01 21:54:34 +0600

    Function Expression

---
+++ Function Expressions/FunctionExpression.js
@@ -0,0 +1,43 @@
+function sayHi() {
+    console.log("hi")}
+let sayHii = function () {
+    console.log("hi")
+}
+
+sayHi()
+sayHii();
+
+//callback function
+
+// function ask(question, yes, no) {
+//   if (confirm(question)) yes();
+//   else no();
+// }
+
+// function showOk() {
+//   alert("You agreed.");
+// }
+
+// function showCancel() {
+//   alert("You canceled the execution.");
+// }
+
+// // usage: functions showOk, showCancel are passed as arguments to ask
+// ask("Do you agree?", showOk, showCancel);
+
+
+// Function Declaration: a function, declared as a separate statement, in the main code flow.
+
+// // Function Declaration
+// function sum(a, b) {
+//   return a + b;
+// }
+
+
+// Function Expression: a function, created inside an expression or inside another syntax construct. Here, the function is created at the right side of the “assignment expression” =:
+
+// // Function Expression
+// let sum = function(a, b) {
+//   return a + b;
+// };
+
